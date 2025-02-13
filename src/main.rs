cfb0d9328e1f411f3aeb660ab41d17e8cfa771b4
Jeremy Gordon <orangej0312@gmail.com>
2025-02-13 15:32:11 -0500

    nix polling crate works thank GOD

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use bincode::deserialize;
 use clap::Parser;
+use nix::poll::{poll, PollFd, PollFlags, PollTimeout};
 use socketry::{bind_listener, connect_channel, make_channels};
 use std::{
+    collections::HashMap,
     fs::File,
     io::Read,
     net::{TcpListener, TcpStream},
+    os::fd::{AsFd, AsRawFd},
     thread::sleep,
     time::Duration,
 };
@@ -42,6 +45,20 @@ fn main() -> Result<(), Reasons> {
 
     let mut listener = bind_listener(&hostname)?;
     let mut outgoing_channels = make_channels(&hostname, &peer_list)?;
+    let mut incoming_channels = HashMap::new();
+    let expected_connections = peer_list.len() - 1;
+    while incoming_channels.len() < expected_connections {
+        if let Ok((mut sock, _)) = listener.accept() {
+            // need this so nix::poll can do its work
+            sock.set_nonblocking(true).map_err(Reasons::IO)?;
+            incoming_channels.insert(sock.as_raw_fd(), sock);
+        }
+    }
+
+    let mut poll_fds: Vec<PollFd> = incoming_channels
+        .iter()
+        .map(|(_, s)| PollFd::new(s.as_fd(), PollFlags::POLLIN))
+        .collect();
 
     // means we're ready to go!
     // project said "no unneccesary prints"
@@ -60,5 +77,34 @@ fn main() -> Result<(), Reasons> {
     let token_delay = Duration::from_secs_f64(args.token_delay.unwrap_or(0.0));
     let marker_delay = Duration::from_secs_f64(args.marker_delay.unwrap_or(0.0));
 
-    loop {}
+    loop {
+        let events = poll(&mut poll_fds, PollTimeout::NONE).map_err(|e| Reasons::IO(e.into()))?;
+
+        if events == 0 {
+            continue;
+        }
+
+        for pfd in poll_fds.iter().filter(|pfd| {
+            pfd.revents()
+                .unwrap_or(PollFlags::empty())
+                .contains(PollFlags::POLLIN)
+        }) {
+            let mut buffer = [0u8; 1024];
+            let b = incoming_channels
+                .get(&pfd.as_fd().as_raw_fd())
+                .unwrap()
+                .read(&mut buffer)
+                .map_err(Reasons::IO)?;
+            let msg: Message = deserialize(&buffer[..b]).map_err(|_| Reasons::BadMessage)?;
+
+            data.recv_message(msg);
+            match msg {
+                Message::Token => {
+                    sleep(token_delay);
+                    data.pass_token(&mut outgoing_channels)?;
+                }
+                _ => {}
+            }
+        }
+    }
 }
